e616a5f4572eb2b23fa2b521aab131a09eb7418b
Berty Pribilovics <bpribilovics@twelvefold.com>
2017-02-08 13:43:01 -0800

    cleaned up files according to PEP8

--- setup.py
+++ setup.py
@@ -9,15 +9,15 @@ A Bottle based REST API.
 from setuptools import setup
 
 setup(
-  name="Micro URL",
-  version="0.1",
-  description="URL shortening service the device type detection and routing",
-  author="Berty Pribilovics",
-  author_email="bpribilovics@gmail.com",
-  packages=["micro_url"],
-  install_requires=[
-    "bottle",
-    "short_url",
-    "user_agents"
-  ]
+    name="Micro URL",
+    version="0.1",
+    description="URL shortening service the device type detection and routing",
+    author="Berty Pribilovics",
+    author_email="bpribilovics@gmail.com",
+    packages=["micro_url"],
+    install_requires=[
+        "bottle",
+        "short_url",
+        "user_agents"
+    ]
 )
