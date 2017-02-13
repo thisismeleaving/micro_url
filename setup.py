0892e3e8975139123e792e825a6d43348b6658c9
bpribilovics <bpribilovics@twelvefold.com>
2017-02-13 10:34:42 -0800

    fixed typo in setup

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 setup(
     name="Micro URL",
     version="0.1",
-    description="URL shortening service the device type detection and routing",
+    description="URL shortening service with device type detection and routing",
     author="Berty Pribilovics",
     author_email="bpribilovics@gmail.com",
     packages=["micro_url"],
